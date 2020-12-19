20b8cac14e7614c9fe4347b0abdc284a07cfe2f4
riqki kamal <riqkikamal@outlook.com>
2020-12-19 21:32:59 +0700

    init

---
+++ src/Pages/loginScreen/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+export default class loginScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> login </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
